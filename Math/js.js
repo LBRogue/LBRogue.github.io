e166524f3c0c904325d00346b748996824bc5c7d
LBRogue <56671333+LBRogue@users.noreply.github.com>
2023-05-07 15:01:51 +0800

    math

---
+++ Math/js.js
@@ -0,0 +1,14 @@
+var cycleCounter = 0;
+var category = ["Addition","Subtraction","Multiplication","Division"]
+function generate(){
+	
+}
+function addition(){
+	var x = Math.floor(Math.random()*999)
+	var y = Math.floor(Math.random()*999)
+	var eq = document.getElementById("equation").innerHTML = x+"+"+y"="
+}
+
+function cycle(){
+	
+}
